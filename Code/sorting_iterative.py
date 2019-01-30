7fa9c97d36aa19eedb071466a8b3548fe586393c
Severiano Badajoz <sevebadajoz@gmail.com>
2019-01-29 16:41:45 -0800

    Create is_sorted

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -6,6 +6,11 @@ def is_sorted(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
+    last = items[0]
+    for num in items:
+        if last > num:
+            return False
+    return true
 
 
 def bubble_sort(items):
@@ -14,8 +19,9 @@ def bubble_sort(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
-
+    while not is_sorted(items):
+        # TODO: Swap adjacent items that are out of order
+        for
 
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
