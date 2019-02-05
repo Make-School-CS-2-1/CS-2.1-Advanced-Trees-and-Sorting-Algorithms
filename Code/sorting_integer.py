a4f0bcd77b0c75d97a9d5ae1042e8a15807d0b3b
Severiano Badajoz <sevebadajoz@gmail.com>
2019-02-05 11:29:02 -0800

    Complete counting_sort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -7,9 +7,25 @@ def counting_sort(numbers):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Find range of given numbers (minimum and maximum integer values)
+    min = numbers[0]
+    max = numbers[0]
+    for num in numbers:
+      if num < min:
+        min = num
+      elif num > max:
+        max = num
     # TODO: Create list of counts with a slot for each number in input range
+    counts = [0] * (max - min + 1)
     # TODO: Loop over given numbers and increment each number's count
+    for num in numbers:
+      counts[num - min] += 1
     # TODO: Loop over counts and append that many numbers into output list
+    input_index = 0
+    for i, num in enumerate(counts):
+      # print(i, num)
+      for j in range(num):
+        numbers[input_index] = i + min
+        input_index += 1
     # FIXME: Improve this to mutate input instead of creating new output list
 
 
